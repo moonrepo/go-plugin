4d2b692661c6c316fb49972e5f46741894944e09
Miles Johnson <mileswjohnson@gmail.com>
2023-08-21 15:36:59 -0700

    Disable test on windows.

--- tests/globals_test.rs
+++ tests/globals_test.rs
@@ -1,4 +1,6 @@
 use proto_pdk_test_utils::*;
 use starbase_sandbox::create_empty_sandbox;
 
+// Currently times out on Windows!
+#[cfg(not(windows))]
 generate_globals_test!("go-test", "golang.org/x/tools/cmd/stringer@latest", "GOBIN");
